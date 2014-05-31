ebe92fb41fc412b64f31eb868c39ac64989a34b6
Santiago Lovón García <santiago.lg.1994@gmail.com>
2014-05-31 13:30:18 -0500

    Finalizacion de Script y graficos

--- AnalisisDeDatos.R
+++ AnalisisDeDatos.R
@@ -6,6 +6,9 @@ nrow(dat[dat$estado == 'ASISTIO',])
 nrow(dat[dat$estado == 'FALTO',])
 
 
+
+
+
 library(ggplot2)
 ggplot(data=edad, aes(x=edad, y=especialidad, group=estado, shape=estado, color=estado)) +    
   geom_line() + geom_point() +  xlab("AÑOS")+ ylab("CITAS") + 
@@ -14,8 +17,13 @@ ggplot(data=edad, aes(x=edad, y=especialidad, group=estado, shape=estado, color=
 asistencias  = edad[edad$estado == 'ASISTIO',]
 faltas = edad[edad$estado == 'FALTO',]
 especialidad = aggregate(edad ~ especialidad + estado, data=dat, FUN=length)
+especialidad = especialidad[order(especialidad$edad , decreasing=TRUE),]
+especialidad$especialidad = factor(especialidad$especialidad)                      
+str(especialidad)
 
-ggplot(especialidad, aes(x=especialidad, y=edad, fill=estado)) + geom_bar(position=position_dodge()) + coord_flip()
-faltas = especialidad[especialidad$estado == 'FALTO',] xlab("ESPECIALIDAD")+ ylab("CANTIDAD")
+ggplot(especialidad, aes(x=especialidad, y=edad, fill=estado)) + 
+  geom_bar(position=position_dodge()) + coord_flip()+ xlab("ESPECIALIDAD")+
+  ylab("CANTIDAD") 
 
-ordenado = faltas[order(faltas$edad , decreasing=TRUE),]
+
+especialidad[especialidad$estado == "ASISTIO", 1]
